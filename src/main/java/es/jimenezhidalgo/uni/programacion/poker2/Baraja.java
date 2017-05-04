24a76f34ee33d69b1385a1c8b0c7a2a2e8d8cadf
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2017-05-05 00:51:22 +0200

    Añadida Baraja

---
+++ src/main/java/es/jimenezhidalgo/uni/programacion/poker2/Baraja.java
@@ -0,0 +1,39 @@
+package es.jimenezhidalgo.uni.programacion.poker2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Created by jesus.jimenez on 08/03/2017.
+ */
+public final class Baraja {
+
+    //Baraja de 20 cartas con ases, doses, sotas, caballos y reyes de los cautro palos
+    private static ArrayList<Carta> mBaraja = new ArrayList<>();
+
+    private static void inicializarBaraja() throws Carta.CartaExcepcion {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 12; j++) {
+                mBaraja.add(new Carta((i * 100) + j));
+            }
+        }
+    }
+
+    /**
+     * Reparte una mano de 5 cartas de la baraja de 20 cartas que tenemos
+     * @return
+     */
+    public static ArrayList<Carta> crearMano() throws Carta.CartaExcepcion {
+        ArrayList<Carta> mano = new ArrayList<>();
+
+        inicializarBaraja();
+        Collections.shuffle(mBaraja);
+
+        for (int i = 0; i < 5; i++) {
+            mano.add(mBaraja.get(i));
+        }
+
+        return mano;
+    }
+}
