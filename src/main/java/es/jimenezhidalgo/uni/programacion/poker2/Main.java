e3c2dbe7bd1777172745425ac80cb60a979feb16
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2017-05-05 01:17:49 +0200

    Añadida clase Main

---
+++ src/main/java/es/jimenezhidalgo/uni/programacion/poker2/Main.java
@@ -0,0 +1,10 @@
+package es.jimenezhidalgo.uni.programacion.poker2;
+
+/**
+ * Created by jesus.jimenez on 05/05/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        //TODO - PROGRAMA PRINCIPAL
+    }
+}
