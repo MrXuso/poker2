df8104bc1ab70be1745671eeb616577c78be729c
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2017-05-05 00:51:30 +0200

    Añadidos tests de Baraja

---
+++ src/test/java/es/jimenezhidalgo/uni/programacion/poker2/BarajaTest.java
@@ -0,0 +1,17 @@
+package es.jimenezhidalgo.uni.programacion.poker2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jesus.jimenez on 05/05/2017.
+ */
+public class BarajaTest {
+
+    @Test
+    public void crearMano() throws Exception {
+    }
+
+}
