2b6320fa65c72dec854f1e4e0eb525cbbbc59511
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2017-05-07 15:09:43 +0200

    Creada clase helper NIFHelper

---
+++ src/main/java/es/jimenezhidalgo/uni/programacion/poker2/utils/NIFHelper.java
@@ -0,0 +1,13 @@
+package es.jimenezhidalgo.uni.programacion.poker2.utils;
+
+/**
+ * Created by jesus.jimenez on 07/05/2017.
+ */
+public class NIFHelper {
+
+    private static final char LETRAS_NIF[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+    private char mLetraNif;
+
+    //TODO implementar la validación de un NIF
+    //letra = letras[NIF%23];
+}
