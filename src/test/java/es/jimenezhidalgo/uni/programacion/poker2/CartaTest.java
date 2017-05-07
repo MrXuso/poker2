0158eeee625ee24c4eecb4fcd6b55f80ecfcb3fc
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2017-05-07 13:27:49 +0200

    Añadidos casos de prueba para las excepciones de las cartas

--- src/test/java/es/jimenezhidalgo/uni/programacion/poker2/CartaTest.java
+++ src/test/java/es/jimenezhidalgo/uni/programacion/poker2/CartaTest.java
@@ -18,6 +18,29 @@ public class CartaTest {
     }
 
     @Test
+    public void cartaExceptionTest(){
+        try {
+            new Carta(400);
+            fail("No se ha detectado la excepción");
+        } catch (Carta.CartaExcepcion ignored) {}
+
+        try {
+            new Carta(215);
+            fail("No se ha detectado la excepción");
+        } catch (Carta.CartaExcepcion ignored) {}
+
+        try {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 12; j++) {
+                    new Carta(i*100 + j);
+                }
+            }
+        } catch (Carta.CartaExcepcion e){
+            fail("Se ha detectado una excepción con una carta correcta");
+        }
+    }
+
+    @Test
     public void toStringTest() throws Exception {
         assertEquals("Expected \"Cuatro de Copas\"", "Cuatro de Copas", carta.toString());
     }
@@ -30,6 +53,12 @@ public class CartaTest {
     @Test
     public void mismoPaloTest() throws Exception {
         assertEquals("Expected true", true, carta.mismoPalo(new Carta(111)));
+    }
+
+    @Test
+    public void getCartaIntTest() throws Exception {
+        assertEquals("Se ha creado una carta 103, pero el valor obtenido no coincide", 103, carta.getCartaInt());
+        assertEquals("Se ha creado una carta 104, pero el valor obtenido no coincide", 104, Carta.getCartaInt(new Carta(104)));
     }
 
 }
