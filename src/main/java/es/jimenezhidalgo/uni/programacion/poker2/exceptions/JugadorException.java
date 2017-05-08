8dda567b2094167b64e586b207482143968118c2
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2017-05-08 23:51:06 +0200

    Cambiadas excepciones

--- src/main/java/es/jimenezhidalgo/uni/programacion/poker2/exceptions/JugadorException.java
+++ src/main/java/es/jimenezhidalgo/uni/programacion/poker2/exceptions/JugadorException.java
@@ -5,7 +5,7 @@ package es.jimenezhidalgo.uni.programacion.poker2.exceptions;
  */
 public class JugadorException extends Exception{
 
-    public static final String JUGADOR_YA_REGISTRADO = "El jugador ya había sido registrado";
+    public static final String JUGADOR_YA_REGISTRADO = "Ese NIF ya ha sido registrado";
 
     public static final String SALDO_INSUFICIENTE = "Saldo insuficiente";
 
@@ -13,7 +13,7 @@ public class JugadorException extends Exception{
 
     public static final String CONTRASENIA_INCORRECTA = "Contraseña incorrecta";
 
-    public static final String EDAD_INCORRECTA = "Edad incorrecta";
+    public static final String EDAD_INCORRECTA = "Debe de tener al menos 18 años para jugar";
 
     public JugadorException() {
         super("Se ha producido un error en el programa");
