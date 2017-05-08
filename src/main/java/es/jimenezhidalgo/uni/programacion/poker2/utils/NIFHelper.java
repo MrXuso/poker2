44c7922a72a4d8f4cb3ad12b900aeb17e2b1ad7c
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2017-05-08 23:55:23 +0200

    Añadida validación dni

--- src/main/java/es/jimenezhidalgo/uni/programacion/poker2/utils/NIFHelper.java
+++ src/main/java/es/jimenezhidalgo/uni/programacion/poker2/utils/NIFHelper.java
@@ -1,13 +1,33 @@
 package es.jimenezhidalgo.uni.programacion.poker2.utils;
 
+import es.jimenezhidalgo.uni.programacion.poker2.exceptions.JugadorException;
+
 /**
  * Created by jesus.jimenez on 07/05/2017.
  */
 public class NIFHelper {
 
     private static final char LETRAS_NIF[] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
-    private char mLetraNif;
 
     //TODO implementar la validación de un NIF
-    //letra = letras[NIF%23];
+    public static char getLetraNif(int Nif){
+        return LETRAS_NIF[Nif % 23];
+    }
+    
+    public static boolean comprobarLetraNif(String Nif) throws JugadorException {
+        char letra;
+        try {
+            int nifNumero = Integer.parseInt(Nif.substring(0, 8));
+            letra = LETRAS_NIF[nifNumero % 23];
+            //letra = letras[NIF%23];
+            if(letra != Nif.toUpperCase().toCharArray()[8]){
+                throw new JugadorException(JugadorException.NIF_INCORRECTO);
+            } else {
+                return true;
+            }
+        } catch (Exception e){
+            throw new JugadorException(JugadorException.NIF_INCORRECTO);
+        }
+    }
+    
 }
