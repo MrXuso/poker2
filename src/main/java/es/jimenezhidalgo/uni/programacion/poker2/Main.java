6acccdf55fdb033a19aab9e21e0de1ec8f05a5a5
Francisco Jesús Jiménez Hidalgo <fjjsus@gmail.com>
2017-05-08 23:52:12 +0200

    Creada función principal

--- src/main/java/es/jimenezhidalgo/uni/programacion/poker2/Main.java
+++ src/main/java/es/jimenezhidalgo/uni/programacion/poker2/Main.java
@@ -1,10 +1,27 @@
 package es.jimenezhidalgo.uni.programacion.poker2;
 
+import es.jimenezhidalgo.uni.programacion.poker2.exceptions.JugadorException;
+import es.jimenezhidalgo.uni.programacion.poker2.gui.VentanaPrincipal;
+import es.jimenezhidalgo.uni.programacion.poker2.utils.NIFHelper;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by jesus.jimenez on 05/05/2017.
  */
 public class Main {
+    
+    public static Apuesta sApuesta = null;
+    
+    /**
+     * Jugador que apuesta, una vez inicie sesión o se registre, le daremos un valor
+     */
+    public static Jugador sJugador = null;
+    
     public static void main(String[] args) {
-        //TODO - PROGRAMA PRINCIPAL
+        
+        //Ventana principal
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
     }
 }
